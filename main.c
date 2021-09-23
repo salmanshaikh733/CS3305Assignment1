f87697e532257e3af7270327de17a7f7c03788dc
Salman <salman.shaikh1678@gmail.com>
2021-09-23 18:08:20 -0400

    commit changes to develop branch- main working branch will merge to main when completely done

--- main.c
+++ main.c
@@ -5,8 +5,18 @@
 #include <stdlib.h>
 
 
-int main() {
+/*
+ * CS3305 - Assignment 1
+ *
+ * Created by Salman Shaikh on 2021-09-23
+ * Student Number mshaik52 - 250959996
+ */
 
+
+//main function
+int main(int argc, const char * argv[]) {
+
+    pid_t child_1,child_2;
 
 
     return 0;
