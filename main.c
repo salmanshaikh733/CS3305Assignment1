7da7fe13df8194a2f93b87617d10ae37dac8f13d
Salman <salman.shaikh1678@gmail.com>
2021-09-24 21:35:17 -0400

    finished strcat and calling external program logic

--- main.c
+++ main.c
@@ -21,25 +21,26 @@ int main(int argc, const char * argv[]) {
 
     pid_t child_1,child_2, child_2_1;
 
-    char c =5;
     //create the child process and they child process pid
     child_1=fork();
 
     //for parent process to execute
     if(child_1>0) {
-        printf("Parent process created child with pid: %d, parent pid is: %d.\n",child_1,getpid());
+        printf("parent (PID %d) created child_1 (PID %d).\n",getpid(),child_1);
+        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2.\n",getpid(),child_1);
     }
 
     //for child process to execute
     if(child_1 == 0) {
-        printf("I am child 1 my pid is: %d, and my parents pid is: %d\n",getpid(),getppid());
+        printf("child_1 (PID %d) is calling an external_program.out and leaving parent\n",getpid(),getppid());
 
+        char externalMessage[100]="";
+        sprintf(externalMessage,"%d",getpid());
+        strcat(externalMessage,"for child_1");
 
-        //TODO call an external program...
-        execl(argv[1], "",NULL);
+        execl(argv[1], "",externalMessage);
         wait(NULL);
     }
-
     //parent will wait for child 1 to finish
     wait(NULL);
 
@@ -48,7 +49,7 @@ int main(int argc, const char * argv[]) {
 
     //for parent
     if(child_2 > 0) {
-        printf("child_2 has been made with pid: %d\n", child_2);
+        printf("parent (PID %d) created child_2 (PID %d)\n", getpid(),child_2);
     }
     //for child 2 process
     if(child_2 ==0 ){
@@ -57,13 +58,18 @@ int main(int argc, const char * argv[]) {
 
         //for child 2
         if(child_2_1 > 0){
-            printf("child_2_1 has been made with pid %d\n", child_2_1);
+            printf("child_2 (PID %d) created child_2.1 (PID %d)\n", getpid(), child_2_1);
+            wait(NULL);
         }
         //for child_2_1
         if(child_2_1 == 0) {
-            //TODO child_2_1 will call external program here
-            //TODO call an external program...
-            execl(argv[1], "",NULL);
+            char externalMessage[100]="";
+            sprintf(externalMessage,"%d",getpid());
+            strcat(externalMessage,"for child_2.1");
+
+            printf("child_2.1 (PID %d) is calling an external program external_program.out and leaving child_2.1\n",getpid());
+
+            execl(argv[1], "",externalMessage);
             wait(NULL);
         }
     }
@@ -73,8 +79,7 @@ int main(int argc, const char * argv[]) {
 
     //if back in parent- meaning both child processes have ended. End the program.
     if(child_1 > 0 && child_2 > 0) {
-        printf("ending process\n");
-        exit(0);
+        printf("child_1 and child_2 are completed and parent process is terminating…\n");
     }
     return 0;
 }
